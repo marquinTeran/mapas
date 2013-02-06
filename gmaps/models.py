5750bacea97876910f42302dcffee6c043081ba3
Marco Teran <marquin_90@hotmail.com>
2013-02-05 23:22:31 -0500

    Creamos un formulario a partir de el modelo antes creado

--- gmaps/models.py
+++ gmaps/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 class Ubicacion(models.Model):
 	nombre = models.CharField(max_length=200)
@@ -10,3 +11,8 @@ class Ubicacion(models.Model):
 
 	def __unicode__(self):
 		return self.nombre
+
+
+class UbicacionForm(ModelForm):
+	class Meta:
+		model = Ubicacion
