1b719315f2c1ac0c976673d80a23438d86758e73
Marco Teran <marquin_90@hotmail.com>
2013-02-05 23:17:02 -0500

    Creamos el modelo ubicacion

--- gmaps/models.py
+++ gmaps/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ubicacion(models.Model):
+	nombre = models.CharField(max_length=200)
+	lat = models.CharField(max_length=50)
+	lng = models.CharField(max_length=50)
+	fecha = models.DateTimeField(auto_now_add=True)
+	user = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.nombre
