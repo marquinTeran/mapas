17c8919e9385e8b431e1fa9a10a963ccc1667298
Marco Teran <marquin_90@hotmail.com>
2013-02-06 01:09:23 -0500

    Interactuamos con el API de google maps V3

--- gmaps/views.py
+++ gmaps/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from gmaps.models import UbicacionForm
 
 def index(request):
-	return render_to_response('index.html', context_instance=RequestContext(request))
+	form = UbicacionForm()
+	return render_to_response('index.html', {'form': form}, context_instance=RequestContext(request))
---
+++ static/js/scripts.js
@@ -0,0 +1,53 @@
+$(function (){
+	if(navigator.geolocation)
+	{
+		navigator.geolocation.getCurrentPosition(getCoords, getError);
+	} else 
+	{
+		initialize(0.357482, -78.11687);
+	}
+
+	function getCoords(positions)
+	{
+		var lat = positions.coords.latitude;
+		var lng = positions.coords.longitude;
+
+		initialize(lat, lng);
+	}
+
+	function getError(err)
+	{
+		initialize(0.357482, -78.11687);
+	}
+
+	function initialize(lat, lng)
+	{
+		var latlng = new google.maps.LatLng(lat, lng);
+		var map;
+		var mapSettings = {
+			center: latlng,
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		map = new google.maps.Map($('#mapa').get(0), mapSettings);
+        
+        var marker = new google.maps.Marker({
+          map:map,
+          draggable:true,
+          animation: google.maps.Animation.DROP,
+          position: latlng,
+          title: 'Arrastrame!!'
+        });
+
+        google.maps.event.addListener(marker, 'position_changed', function(){
+        	getMarkerCoords(marker);
+        });
+
+        function getMarkerCoords(marker)
+        {
+        	var markerCoords = marker.getPosition();
+        	$("#id_lat").val( markerCoords.lat() );
+        	$("#id_lng").val( markerCoords.lng() );
+        }
+	}
+});
